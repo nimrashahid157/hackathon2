7839b24526f2e2a1b39d2c4006dfe581daaf136d
nimrashahid157 <140234341+nimrashahid157@users.noreply.github.com>
2024-03-30 11:56:07 -0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,44 @@
+document.getElementById("btn").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var repeatPassword = document.getElementById("repeatPassword").value;
+  
+    if (!validateFirstName(firstName)) {
+      alert("First name must be at least 3 characters and not exceed 20 characters.");
+      return;
+    }
+  
+    if (!validateLastName(lastName)) {
+      alert("Last name must be at least 2 characters and not exceed 20 characters.");
+      return;
+    }
+  
+    if (!validatePassword(password)) {
+      alert("Password must be at least 8 characters and include capital and small letters.");
+      return;
+    }
+  
+    if (password !== repeatPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+  
+    alert("Signup successful!");
+  });
+  
+  function validateFirstName(firstName) {
+    return /^[a-zA-Z]{3,20}$/.test(firstName);
+  }
+  
+  function validateLastName(lastName) {
+    return /^[a-zA-Z]{2,20}$/.test(lastName);
+  }
+  
+  function validatePassword(password) {
+    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
+  }
+  
